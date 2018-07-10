0283344b403ac61347a84692668e26f26d0a67aa
Xuehai-Chen <13162576651@163.com>
2018-07-10 16:18:51 +0800

    init commit

---
+++ sim/cmake-build-debug/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/misc/examples.c.o"
+  "CMakeFiles/sim.dir/misc/isa.c.o"
+  "CMakeFiles/sim.dir/misc/node.c.o"
+  "CMakeFiles/sim.dir/misc/outgen.c.o"
+  "CMakeFiles/sim.dir/misc/yas-grammar.c.o"
+  "CMakeFiles/sim.dir/misc/yas.c.o"
+  "CMakeFiles/sim.dir/misc/yis.c.o"
+  "CMakeFiles/sim.dir/pipe/ncopy.c.o"
+  "CMakeFiles/sim.dir/pipe/psim.c.o"
+  "CMakeFiles/sim.dir/seq/ssim.c.o"
+  "sim.pdb"
+  "sim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
